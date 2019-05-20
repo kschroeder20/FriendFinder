44de6bf0acce016888ac211e9bbe80e87f34ff1f
Kevin Schroeder <schroederkevin94@gmail.com>
2019-05-20 08:58:37 -0400

    <message>

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.port || 3000;
+var PORT = process.env.port || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({
