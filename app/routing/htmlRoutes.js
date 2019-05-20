e486404af7af8a7d9cf9bf28b647b922fdbf3500
Kevin Schroeder <schroederkevin94@gmail.com>
2019-05-20 08:41:53 -0400

    ready to deploy

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -11,6 +11,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/contact.html"));
     });
 
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+
     app.get("*", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
